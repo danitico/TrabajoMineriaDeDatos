f3ca69917dc23fa60570897c4209992acf0f35fc
F-Vigo <49415023+F-Vigo@users.noreply.github.com>
2022-01-01 18:43:41 +0100

    Segundo commit (ejemplo)

--- src/utils/functions.R
+++ src/utils/functions.R
@@ -11,8 +11,6 @@ collapse_labels = function(dataframe, col_index_1, col_index_2) {
   new_dataframe = dataframe[,-c(col_index_1, col_index_2)] %>% as.data.frame()
   colnames(new_dataframe) = colnames(dataframe)[-c(col_index_1, col_index_2)]
   
-  
-  #  as.data.frame(col.names = as.character())
   new_dataframe$target = collapse_two_binaries(
     dataframe[,col_index_1],
     dataframe[,col_index_2]
@@ -27,7 +25,7 @@ split_target = function(probs_dataframe) {
   # Assumed probs_dataframe has a row per instance and is of the form (id, prob(0,0), prob(0,1), prob(1,0), prob(1,1)).
   
   new_dataframe = apply(probs_dataframe, 1, function(row) c(
-      row[1], # id.
+      row[1], # id
       row[4]+row[5], # p(10)+p(11)
       row[3]+row[5]  # p(01)+(11)
     )) %>% t()
