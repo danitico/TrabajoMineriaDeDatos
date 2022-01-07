8cf5206c74e6dab8e47cd713c4c5fc1ee9fac904
F-Vigo <49415023+F-Vigo@users.noreply.github.com>
2022-01-07 12:39:17 +0100

    Añadido knn

---
+++ src/knn/knn_distances.R
@@ -0,0 +1,59 @@
+library(magrittr)
+
+
+# Auxiliar
+base_hamming_distance <- function(x1, x2) {
+  as.integer(x1 != x2)
+}
+
+
+# Auxiliar
+generalised_hamming_distance <- function(x1, x2) {
+  ifelse(
+    is.ordered(x1), # Se asume que x2 es de la misma clase
+    abs(as.integer(x1) - as.integer(x2)) / (length(levels(x1)) - 1), # Se divide para escalar la distancia máxima a 1
+    as.integer(x1 != x2)
+  )
+}
+
+
+
+hamming_distance <- function(x, y, is_generalised) {
+  
+  component_distance <- function(x1, x2) {
+    ifelse(
+      is_generalised,
+      generalised_hamming_distance(x1, x2),
+      base_hamming_distance(x1, x2)
+    ) %>% ifelse(is.na(.), 1, .)
+  }
+
+  sapply(
+    1:length(ncol(x)),
+    function(i) component_distance(x[[i]], y[[i]])
+  ) %>% sum()
+}
+
+
+get_hamming_distance <- function(is_generalised) {
+  function(x, y) hamming_distance(x, y, is_generalised)
+}
+
+
+
+gower_distance <- function(x, y) {
+  
+  component_distance <- function(column) {
+    x1 <- column[1]
+    x2 <- column[2]
+    as.integer(x1 == x2)
+  }
+  
+  numerator <- apply(
+    rbind(x, y),
+    2,
+    component_distance
+  )
+  
+  sum(numerator, na.rm = T) / sum(is.na(numerator))
+}
